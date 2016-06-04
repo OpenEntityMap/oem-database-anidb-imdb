0e26551898cf839e19e2042213154cc94f7fd8f9
Dean Gardiner <me@dgardiner.net>
2016-06-04 14:56:18 +1200

    Imported fuzeman/anime-lists@e042efc769cede200f435497dc0722623a1ce8d6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="oem-database-anidb-imdb",
-    version="1.16.0",
+    version="1.16.1",
     author="Dean Gardiner",
     author_email="me@dgardiner.net"
 )
