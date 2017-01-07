f0680942d653338ed1432f3db9751d7873b6ad38
fm-build <fuzeman91+fm-build@gmail.com>
2017-01-08 11:40:09 +1300

    Bumped version to 1.17.6

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PACKAGE_NAME = 'oem-database-anidb-imdb'
 def build_config():
     config = {
         'name': PACKAGE_NAME,
-        'version': '1.17.5',
+        'version': '1.17.6',
 
         'author': 'Dean Gardiner',
         'author_email': 'me@dgardiner.net',
